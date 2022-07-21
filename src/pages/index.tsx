4261d0997625496be03a23d177e0c66e32c83f31
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-07-21 15:26:12 +0900

    不要箇所を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,14 @@
-import { graphql, PageProps } from 'gatsby';
 import React from 'react';
-// import { PageProps } from 'gatsby';
-// import { ArticleCard } from '../components/Article/ArticleCard';
+import { graphql, PageProps } from 'gatsby';
+
 import { ArticleCardList } from '../components/Article/ArticleCardList';
-import { GatsbyImageGenerator } from '../components/GatsbyImageGenerator';
-// import { GatsbyImageConverter } from '../components/GatsbyImageConverter';
 import { Header } from '../components/Header/Header';
-// import { Header } from '../components/Header/Header';
-// import { microcmsDataType } from '../models/microcms';
 
 const Home: React.FC<PageProps<Queries.IndexQuery>> = ({ data }) => {
   console.log(data.allMicrocmsBlogs.edges);
   return (
     <>
       <Header />
-      <GatsbyImageGenerator
-        url="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/b9558350a7f3444482b037d833a7bccc/Untitled%20(1).png"
-        alt=""
-      />
       <title>Create Next App</title>
       <main>
         <ArticleCardList articleCardList={data.allMicrocmsBlogs.edges} />
