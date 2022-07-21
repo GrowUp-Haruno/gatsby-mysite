6607e5062037f5f190ed80a09d96ef4135466793
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-07-21 15:26:01 +0900

    CSSを適用

--- src/templates/articleTemplate.tsx
+++ src/templates/articleTemplate.tsx
@@ -1,14 +1,20 @@
-import { graphql, PageProps } from 'gatsby'
-import React, { FC } from 'react'
+import { graphql, PageProps } from 'gatsby';
+import React, { FC } from 'react';
+import { Header } from '../components/Header/Header';
 import { stringHtmlToJsxElement } from '../libs/stringHtmlToJsxElement';
 
 const ArticleTemplate: FC<PageProps<Queries.ArticleTemplateQuery>> = ({ data }) => {
-  
   if (!data) return null;
   if (!data.microcmsBlogs || !data.microcmsBlogs.content) return null;
-    const blog = stringHtmlToJsxElement(data.microcmsBlogs.content);
-  console.log(blog);
-  return <>{blog}</>;
+  const blog = stringHtmlToJsxElement(data.microcmsBlogs.content);
+
+  return (
+    <>
+      <Header />
+      <title>Create Next App</title>
+      <main>{blog}</main>
+    </>
+  );
 };
 
 export const query = graphql`
@@ -19,5 +25,4 @@ export const query = graphql`
   }
 `;
 
-
-export default ArticleTemplate
+export default ArticleTemplate;
